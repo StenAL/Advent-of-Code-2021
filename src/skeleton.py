40bcde58bca254b65616e2d90796798fe4ed626b
Sten Laane <sten@laane.xyz>
2021-12-02 19:15:53 +0000

    Add skeleton and util files

---
+++ src/skeleton.py
@@ -0,0 +1,19 @@
+from util import *
+from collections import *
+import copy
+
+day = 1
+
+
+def task1():
+    data = get_input_for_day(day)
+    # data = get_input_for_file("test")
+
+
+def task2():
+    data = get_input_for_day(day)
+    # data = get_input_for_file("test")
+
+
+task1()
+# task2()
