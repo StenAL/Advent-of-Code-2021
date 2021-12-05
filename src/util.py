5bd7c9f72ba0429f5fa37bdfc1944d4725dd8432
Sten Laane <sten@laane.xyz>
2021-12-05 15:50:37 +0000

    Add types to util functions

--- src/util.py
+++ src/util.py
@@ -1,18 +1,18 @@
-def get_input_for_day(day):
+def get_input_for_day(day: int) -> list[str]:
     f = open("input/day" + str(day) + ".txt")
     return [line.strip() for line in f.readlines()]
 
 
-def get_int_input_for_day(day):
+def get_int_input_for_day(day: int) -> list[int]:
     f = open("input/day" + str(day) + ".txt")
     return [int(line.strip()) for line in f.readlines()]
 
 
-def get_input_for_file(file):
+def get_input_for_file(file: str) -> list[str]:
     f = open("input/" + file + ".txt")
     return [line.strip() for line in f.readlines()]
 
 
-def get_int_input_for_file(file):
+def get_int_input_for_file(file: str) -> list[int]:
     f = open("input/" + file + ".txt")
     return [int(line.strip()) for line in f.readlines()]
