72157b5fd7b6ab38fceb7711fdf7452f5119a43b
Sten Laane <sten@laane.xyz>
2021-12-06 19:18:11 +0000

    Stage new files created by fetchInput in git

--- fetchInput.sh
+++ fetchInput.sh
@@ -3,6 +3,7 @@ inputFile="src/input/day$day.txt"
 if [[ ! -f $inputFile ]]; then
   curl -sS --cookie "session=$(cat cookie.txt)" "https://adventofcode.com/2021/day/$day/input" -o $inputFile
   echo "Fetched $inputFile"
+  git add $inputFile
 else
   echo "$inputFile already exists"
 fi
@@ -12,6 +13,7 @@ if [[ ! -f $sourceFile ]]; then
   cp src/skeleton.py src/day$day.py
   sed -i "s/'REPLACE_ME'/$day/g" src/day$day.py
   echo "Created $sourceFile"
+  git add $sourceFile
 else
   echo "$sourceFile already exists"
 fi
