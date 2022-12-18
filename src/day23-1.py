68498f4241587f99cf678007634897ee8b4d8848
Sten Laane <21343173+StenAL@users.noreply.github.com>
2022-12-18 13:04:59 +0200

    Day 23 part 1 improvements
    No more distance pairs hardcoding, DFS instead of using a priority
    queue, going straight to room if possible

--- src/day23-1.py
+++ src/day23-1.py
@@ -35,28 +35,28 @@ reachability_map = {"H1": {"H2"}, "H2": {"H1", "H3", "A1"}, "H3": {"H2", "H4", "
                     "D1": {"D2", "H5", "H6"}, "A2": {"A1"}, "B2": {"B1"}, "C2": {"C1"},
                     "D2": {"D1"}}
 
-distance_map = {
-    ("H1", "A1"): 3, ("H1", "A2"): 4, ("H1", "B1"): 5, ("H1", "B2"): 6, ("H1", "C1"): 7, ("H1", "C2"): 8,
-    ("H1", "D1"): 9, ("H1", "D2"): 10,
-    ("H2", "A1"): 2, ("H2", "A2"): 3, ("H2", "B1"): 4, ("H2", "B2"): 5, ("H2", "C1"): 6, ("H2", "C2"): 7,
-    ("H2", "D1"): 8, ("H2", "D2"): 9,
-    ("H3", "A1"): 2, ("H3", "A2"): 3, ("H3", "B1"): 2, ("H3", "B2"): 3, ("H3", "C1"): 4, ("H3", "C2"): 5,
-    ("H3", "D1"): 6, ("H3", "D2"): 7,
-    ("H4", "A1"): 4, ("H4", "A2"): 5, ("H4", "B1"): 2, ("H4", "B2"): 3, ("H4", "C1"): 2, ("H4", "C2"): 3,
-    ("H4", "D1"): 4, ("H4", "D2"): 5,
-    ("H5", "A1"): 6, ("H5", "A2"): 7, ("H5", "B1"): 4, ("H5", "B2"): 5, ("H5", "C1"): 2, ("H5", "C2"): 3,
-    ("H5", "D1"): 2, ("H5", "D2"): 3,
-    ("H6", "A1"): 9, ("H6", "A2"): 8, ("H6", "B1"): 7, ("H6", "B2"): 6, ("H6", "C1"): 5, ("H6", "C2"): 4,
-    ("H6", "D1"): 3, ("H6", "D2"): 2,
-    ("H7", "A1"): 10, ("H7", "A2"): 9, ("H7", "B1"): 8, ("H7", "B2"): 7, ("H7", "C1"): 6, ("H7", "C2"): 5,
-    ("H7", "D1"): 4, ("H7", "D2"): 3
-}
+double_distance_rooms = {"H2", "H3", "H4", "H5", "H6", "A1", "B1", "C1", "D1"}
+distance_map: dict[(str, str): int] = {}
+for room in reachability_map:
+    q = [(room, 0)]
+    while len(q) > 0:
+        (destination, distance) = q.pop(0)
+        reachable = reachability_map[destination]
+        for el in reachable:
+            if room != el and (room, el) not in distance_map:
+                d = distance + 1
+                if {destination, el}.issubset(double_distance_rooms):
+                    d = distance + 2
+                q.append((el, d))
+                distance_map[(room, el)] = d
+ROOM_DEPTH = 2
 
 
 class State:
 
     def __init__(self, agents: set[Agent], cost: int):
         self.agents = agents
+        self.location_map: dict[str, Agent] = {agent.location: agent for agent in self.agents}
         self.cost = cost
 
     def __eq__(self, other):
@@ -66,20 +66,29 @@ class State:
         return cost_eq and agents == other_agents
 
     def __lt__(self, other):
-        return True
+        return self.cost < other.cost
 
     def __hash__(self):
         agents = ((agent.location, agent.goal) for agent in self.agents)
-        return hash(self.cost) * hash(agents)
+        return hash((self.cost, agents))
 
     def is_agent_in_final_position(self, agent: Agent):
-        if agent.location == agent.goal + "2":
-            return True
-        other = next(a for a in self.agents if a.goal == agent.goal and a != agent)
-        return other.location == agent.goal + "2" and agent.location == agent.goal + "1"
+        if not agent.location.startswith(agent.goal):
+            return False
+        others_deeper_in_room = set()
+        for i in range(ROOM_DEPTH):
+            a = self.location_map.get(agent.goal + str(i + 1))
+            if a is None or a == agent:
+                continue
+            if a.goal != agent.goal:
+                return False
+            if a.location > agent.location:
+                others_deeper_in_room.add(a)
+
+        return len(others_deeper_in_room) == ROOM_DEPTH - int(agent.location[-1])
 
     def get_reachable_locations(self, origin: str) -> set[str]:
-        occupied_locations = {agent.location for agent in self.agents}
+        occupied_locations = self.location_map.keys()
         reachable_locations = set()
         q = [origin]
         while len(q) > 0:
@@ -91,48 +100,39 @@ class State:
                     reachable_locations.add(el)
         return reachable_locations
 
-    def get_agent_in_location(self, location: str) -> str | None:
-        a = [agent for agent in self.agents if agent.location == location]
-        if len(a) == 1:
-            return a[0].goal
-        return None
+    def is_room_occupied_by_incorrect_agent(self, room: str) -> bool:
+        agents_in_room = {self.location_map.get(room + str(i + 1)) for i in
+                          range(ROOM_DEPTH)}
+        return any(a is not None and a.goal != room for a in agents_in_room)
 
     def get_actions(self):
-        neighbors: dict[str, list[str]] = defaultdict(list)
-        for el in distance_map.keys():
-            neighbors[el[1]].append(el[0])
-            neighbors[el[0]].append(el[1])
         actions = []
         for agent in self.agents:
             if self.is_agent_in_final_position(agent):
                 continue
-            neighboring = neighbors[agent.location]
-            if not agent.location.startswith("H"):
-                potential_destinations = {location for location in neighboring if location.startswith("H")}
+
+            reachable_locations = self.get_reachable_locations(agent.location)
+            room_occupied_by_incorrect_agent = self.is_room_occupied_by_incorrect_agent(agent.goal)
+            reachable_room_locations = {location for location in reachable_locations if
+                                        location.startswith(agent.goal)}
+            should_go_to_room = (not room_occupied_by_incorrect_agent) and len(reachable_room_locations) > 0
+
+            if should_go_to_room:
+                potential_destinations = {max(reachable_room_locations)}
+            elif agent.location.startswith("H"):
+                potential_destinations = set()
             else:
-                potential_destinations = {location for location in neighboring if location.startswith(agent.goal)}
+                potential_destinations = {location for location in reachable_locations if location.startswith("H")}
 
-            reachable_destinations = self.get_reachable_locations(agent.location)
-            potential_destinations = potential_destinations.intersection(reachable_destinations)
-
-            # don't go to A1 if A2 is occupied by B or C or D
-            if self.get_agent_in_location(agent.goal + "2") != agent.goal:
-                potential_destinations.discard(agent.goal + "1")
-            # don't go to A1 if A2 is free
-            if agent.goal + "2" in potential_destinations:
-                potential_destinations.discard(agent.goal + "1")
-
-            # print(agent.goal, potential_destinations)
             for dest in potential_destinations:
-                distance = distance_map[(agent.location, dest)] if (agent.location, dest) in distance_map else distance_map[
-                    (dest, agent.location)]
+                distance = distance_map[(agent.location, dest)]
                 actions.append(Action(agent, dest, distance * agent.move_cost))
-        actions = sorted(actions, key=lambda a: a.cost)
-        # print([str(a) for a in actions])
         return actions
 
     def apply(self, action: Action):
-        agent = next(agent for agent in self.agents if action.agent.location == agent.location)
+        agent = self.location_map[action.agent.location]
+        del self.location_map[agent.location]
+        self.location_map[action.to] = agent
         agent.location = action.to
         self.cost += action.cost
 
@@ -160,31 +160,33 @@ class State:
                 if agent.location.startswith("H"):
                     cost += distance_map[(agent.location, agent.goal + "1")] * agent.move_cost
                 else:
-                    closest_hallway_distance = 20
-                    closest_hallway = "H1"
+                    closest_hallway_distance = max(distance_map.values())
                     for i in range(1, 8):
-                        d = distance_map[("H" + str(i), agent.location)] + distance_map[("H" + str(i), agent.goal + "1")]
-                        if d < closest_hallway_distance:
-                            closest_hallway_distance = min(d, closest_hallway_distance)
-                            closest_hallway = "H" + str(i)
+                        d = distance_map[("H" + str(i), agent.location)] + distance_map[
+                            ("H" + str(i), agent.goal + "1")]
+                        closest_hallway_distance = min(d, closest_hallway_distance)
                     cost += closest_hallway_distance * agent.move_cost
         return cost
 
 
 def search(state: State):
-    q = [(state.h(), state)]
+    q = [state]
+    seen = set()
+    best = 10000000000
     while len(q) > 0:
-        (priority, state) = heapq.heappop(q)
-        print(len(q), priority)
+        state = q.pop()
+        seen.add(state)
+        print(len(q), best)
         if state.is_finished():
-            return priority
+            best = min(best, state.cost)
         actions = state.get_actions()
         for action in actions:
             neighbor = copy.deepcopy(state)
             neighbor.apply(action)
             tentative_cost = neighbor.cost + neighbor.h()
-            heapq.heappush(q, (tentative_cost, neighbor))
-
+            if neighbor not in seen and tentative_cost < best:
+                q.append(neighbor)
+    return best
 
 
 def task1():
@@ -209,10 +211,9 @@ def task1():
     # agents = {Agent("A", "A2"), Agent("A", "D2"), Agent("B", "A1"), Agent("B", "C1"), Agent("C", "B1"),
     #           Agent("C", "C2"), Agent("D", "B2"), Agent("D", "D1")}
     starting_state = State(agents, 0)
-    # starting_state.print()
-    # print(starting_state.h())
     ans = search(starting_state)
     print(ans)
+
 
 def task2():
     data = get_input_for_day(day)
